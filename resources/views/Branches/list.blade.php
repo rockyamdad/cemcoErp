b55847885f8115b018c68d207bc34a9774153f25
robeen <zhdrbn@gmail.com>
2015-09-20 00:28:09 +0600

    User and Branch Section Check

--- resources/views/Branches/list.blade.php
+++ resources/views/Branches/list.blade.php
@@ -4,16 +4,16 @@
     <div class="col-md-12">
         <!-- BEGIN PAGE TITLE & BREADCRUMB-->
         <h3 class="page-title">
-            User Section
+            Branch Section
         </h3>
         <ul class="page-breadcrumb breadcrumb">
             <li>
                 <i class="fa fa-home"></i>
-                <a href="{{URL::to('dasboard/')}}">Home</a>
+                <a href="{{ URL::to('dashboard/') }}">Home</a>
                 <i class="fa fa-angle-right"></i>
             </li>
 
-            <li><a href="{{URL::to('branchList')}}">Branch Listt</a></li>
+            <li><a href="{{URL::to('branchList')}}">Branch List</a></li>
         </ul>
         <!-- END PAGE TITLE & BREADCRUMB-->
     </div>
