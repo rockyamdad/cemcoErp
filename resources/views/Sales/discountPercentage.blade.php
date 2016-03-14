12b4b1e4abef129867b2ffde6a34624bde756411
Aasim <ahsan.aasim@gmail.com>
2016-03-15 03:11:20 +0600

    CV DV aasim

---
+++ resources/views/Sales/discountPercentage.blade.php
@@ -0,0 +1,62 @@
+<script>
+
+</script>
+<div class="modal-dialog shape" id="modal">
+    <div class="modal-content">
+        <div class="modal-header">
+            <button type="button" onclick="closeModal()" class="close" data-dismiss="modal" aria-hidden="true"></button>
+            <h3>Discount Percentage</h3>
+        </div>
+        <div class="modal-body">
+            {!!Form::open(array('url' => '/saveReceive', 'method' => 'post', 'class'=>'form-horizontal payment_form',
+            ))!!}
+            <div class="form-body">
+
+                <div class="alert alert-danger display-hide">
+                    <button data-close="alert" class="close"></button>
+                    You have some form errors. Please check below.
+                </div>
+
+
+                <div class="portlet-body form" id="testt">
+
+                    <!-- BEGIN FORM-->
+                    <div class="form-body">
+                        <div class="col-md-7">
+                            {!!Form::text('discount_percentage',$sales->discount_percentage,array('placeholder' => 'Discount Percentage', 'class' =>
+                            'form-control','id'=>'discount_percentage'))!!}
+
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+            {!!Form::close()!!}
+
+                      <div class="modal-footer">
+                          <button type="button" class="btn blue" onclick="addDiscountPercentage();">Save changes</button>
+                          <button type="button" data-dismiss="modal" class="btn">Close</button>
+                       </div>
+
+        </div>
+    </div>
+</div>
+
+
+<script>
+
+    function addDiscountPercentage(){
+        var discount_percentage = $('#discount_percentage').val();
+        $.ajax({
+            type: "get",
+            url: "savediscount/{{$sales->id}}",
+            data:{'data':discount_percentage},
+            dateType: 'json',
+            success: function (data) {
+                $('#modal').modal('toggle').on('hidden.bs.modal', functionThatEndsUpDestroyingTheDOM);
+                $('.modal-backdrop').hide();
+            }
+        });
+    }
+</script>
--- resources/views/Sales/list.blade.php
+++ resources/views/Sales/list.blade.php
@@ -173,8 +173,11 @@
                                                 <a class="btn green btn-sm sale" style="background-color:#009999" rel="{{ $sale->invoice_id }}"  href="{{ URL::to('sales/sale/'. $sale->invoice_id ) }}" onclick="return confirm('Are you sure you want to Sale this item?');">
                                                     Confirm</a>
                                             @endif
-                                            <a class="btn dark btn-sm" rel="{{ $sale->invoice_id }}" data-toggle="modal"  data-target="#sale" href="{{ URL::to('sales/details/'. $sale->invoice_id ) }}" >
-                                                <i class="fa fa-eye"></i> Detail</a>
+                                                <a class="btn dark btn-sm" rel="{{ $sale->invoice_id }}" data-toggle="modal"  data-target="#sale" href="{{ URL::to('sales/details/'. $sale->invoice_id ) }}" >
+                                                    <i class="fa fa-eye"></i> Detail</a>
+
+                                                <a class="btn dark btn-sm" rel="{{ $sale->invoice_id }}" data-toggle="modal"  data-target="#sale" href="{{ URL::to('sales/discount/'. $sale->id ) }}" >
+                                                    <i class="fa fa-eye"></i> Add Discount Percentage</a>
 
                                             @if($sale->status != 'Completed' && $sale->is_sale == 1)
                                                 <a class="btn blue btn-sm" href="{{ URL::to('sales/showinvoice/'. $sale->invoice_id ) }}">Show Invoice</a>
